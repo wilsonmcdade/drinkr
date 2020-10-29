4fd9980fe8f8a6045c7b0dc7789305a45f6f59b7
wilsonmcdade <wilson.mcdade@gmail.com>
2020-10-29 23:30:41 +0000

    Fully works. Had to modify pump pins because I was going by the GPIO names not the pin names

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ from kivy.lang.builder import Builder
 from kivy.core.window import Window
 
 #global initialization
-dutycycle = 0.5
+DUTY = 5
 
 # pin initialization
 pumppins = {
-    '1': 12,
-    '2': 13,
-    '3': 18,
-    '4': 19
+    '1': 32,
+    '2': 33,
+    '3': 35,
+    '4': 18
 }
 
 GPIO.setmode(GPIO.BOARD)
@@ -41,12 +41,22 @@ pumpsig = []
 for pump in pumppins.values():
     pumpsig.append((GPIO.PWM,pump,0.05))
 
+# Configure logging modes, format
 def log_setup():
-    logging.basicConfig(filename='drinkr.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename='drinkr.log', filemode='w', format='%(asctime)s\
+     - %(levelname)s - %(message)s')
     logging.info('Beginning Log')
     return
 
+# Kivy app
 class drinkr(App):
+
+    '''
+        Kivy build function
+        Initiates config and settings, loads the drinkr.kv file, names buttons based on the config file,
+            and sets the background color.
+        returns a widget structure.
+    '''
     def build(self):
         config = self.config
         self.settings_cls = SettingsWithSidebar
@@ -54,19 +64,31 @@ class drinkr(App):
         root = Builder.load_file('drinkr.kv')
         
         labels = root.ids
-        idx = 1
+        idx = 0
         for labeled, label in labels.items():
             label.text = self.config.get(str(idx),'name')
+            if str(idx) not in config.sections():
+                logging.info("Index %i not in config sections",idx)
             idx += 1
+
+
+        global DUTY
+        DUTY = int(self.config.get('general','speed'))
 
         Window.clearcolor = (.85,.85,.85,1)
 
         return root
 
+    '''
+        build_config
+        Basically just sets the default values, to be inserted into the 
+		drinkr.ini. These get overwritten whenever the settings are
+		changed so they're not entirely important
+    '''
     def build_config(self, config):
         config.setdefaults(
                 'general',{
-                    'speed':2,
+                    'speed':5,
                     'debug':0
                     })
         config.setdefaults(
@@ -90,42 +112,74 @@ class drinkr(App):
                     'name':'Drink 4'
                     })
  
-    def pump(self,num):
+    '''
+        Custom pump function, pumps from a specific pump
+        Checks if the pump number is valid, calls pumpsig function to change the
+		pumps duty cycle, starting pump
+    '''
+    def pump(self,num,mode):
         if str(num) in pumppins.keys():
-            pumpsig[int(num)][0](*pumpsig[int(num)][1:])
-            logging.info('Pumping')
-            logging.debug('Pumping pump %i', num)
-        else:
-            logging.info('Num not in Pumps: %i',num)
-            logging.debug('Pump %i not in pumps list', num)
-   
-    #at some point make a settings button and call this
-    def build_settings(self,settings):
-        settings.add_json_panel('Settings', self.config, 'drinks.json')
+            if mode is 1:
+                pump = pumpsig[int(num)-1][0](*pumpsig[int(num)-1][1:])
+                pump.start(DUTY)
+                pump.ChangeDutyCycle(15*DUTY)
+                logging.info('Pump: Pumping %i', int(num))
+            else:
+                pump = pumpsig[int(num)-1][0](*pumpsig[int(num)-1][1:])
+                pump.ChangeDutyCycle(0)
+                pump.stop()
+                logging.info('Pump: Stopping pump %i', int(num))
 
+        else:
+            logging.info('Pump: Num not in Pumps: %i',int(num))
+   
+    '''
+        builds settings panels
+    '''
+    def build_settings(self,settings):
+        settings.add_json_panel('Settings', self.config, '/home/pi/drinkr/drinks.json')
+
+    '''
+        on_config_change changes the config file
+        Sees a config change, checks if it's valid, and makes changes to the config file.
+    '''
     def on_config_change(self, config, section, key, value):
-        if (section not in pumppins.keys()) or (section != 'general'):
+        if (section not in pumppins.keys()) and (section != 'general'):
             logging.info('Something went wrong.')
-            logging.debug('Config section not valid: Section: %s, Key: %s, Value: %s',section,key,value)
+            logging.debug('Config: section not valid: Section: %s, Key: %s, Value: %s',section,key,value)
         else :
+            logging.debug('Config: Section; %s, Key: %s, Value: %s',section,key,value)
             if section is 'general':
                 if key is 'speed':
-                    if int(value) > 5 or int(value) < 0:
-                        logging.info('Speed invalid')
-                        logging.debug('Invalid Speed: %s',value)
+                    if int(value) > 5 and int(value) < 0:
+                        self.popup = Popup(title="Error", content=Label(text='Please keep the speed between 0 and 5.'))
+                        popup.open()
+                        logging.info('Config: Speed invalid')
+                        logging.debug('Config: Invalid Speed: %s',value)
                     else: 
-                        global dutycycle
-                        dutycycle = value/5
-                if key is 'debug':
+                        global DUTY
+                        DUTY = int(value)/5
+                        logging.info('Config: Duty cycle is now %i',int(value)/5)
+                elif key is 'debug':
                     if value is '0':
                         logging.setLevel('INFO')
                     else:
                         logging.setLevel('DEBUG')
-        
+                else:
+                    logging.info('Config: Key not found: %s',key)
+            else:
+                if section is '1':
+                    if key is 'enable':
+                        if value is '0':
+                            #disable pump
+                            print(self)
+                 
     def close_settings(self, settings=None):
+        logging.info('Settings: Closing settings')
         super(drinkr, self).close_settings(settings)
-        print(f'exec: {sys.executable} {["python"] + sys.argv}')
-        os.execvp(sys.executable, ['python'] + sys.argv)
+        os.system('python3 /home/pi/drinkr/main.py')
+	#print(f'exec: {sys.executable} {["python3"] + sys.argv}')
+        #os.execvp(sys.executable, ['python3'] + sys.argv)
 
 
 
